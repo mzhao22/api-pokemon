7a635a8be18250fd799c69a85d3ffd6a8adafb50
Mia Mia <aceline0326@gmail.com>
2017-10-11 16:55:38 -0500

    added api link

--- site.js
+++ site.js
@@ -1,9 +1,26 @@
 
 
  $('#user-form').on("submit", function(e){
-	 //var pokemon = $('#pokemon').val();
-     e.preventDefault();
 	 var pokemon = $('#pokemon').val();
+  $.ajax({
+    type: "GET",
+    url: 'https://pokeapi.co/api/v2/pokemon/' + input + '&maxResults=20',
+    dataType: "html",
+    success: function (results){
+     var data = JSON.parse(results);
+     if (Array.isArray(data.items)) {
+      console.log(data.items);
+      parseData(data);
+    } else {
+      console.log('single object: ');
+  // console.log(data);
+      }
+    }
+  });
+  e.preventDefault();
+});
+
+
 
    if ($('#pokemon').val()==''){	 }
 
