ccb12bceefc005d2fbf737db9103952fc7ac6fda
Mia Mia <aceline0326@gmail.com>
2017-10-13 13:30:06 -0500

    parsed something wrong

--- site.js
+++ site.js
@@ -1,34 +1,29 @@
+var num=0;
 
-
- $('#user-form').on("submit", function(e){
-	 var pokemon = $('#pokemon').val();
-  $.ajax({
-    type: "GET",
-    url: 'https://pokeapi.co/api/v2/pokemon/' + pokemon + '&maxResults=20',
-    dataType: "html",
-    success: function (results){
-     var data = JSON.parse(results);
-     if (Array.isArray(data.items)) {
-      console.log(data.items);
-      parseData(data);
-    } else {
-      console.log('single object: ');
-  // console.log(data);
+$('#user-form').on('submit', function(e) {
+  var pokename = $('#poke').val();
+    $.ajax({
+      type: "GET",
+      url: 'https://pokeapi.co/api/v2/pokemon/' + pokename,
+      dataType: "JSON",
+      success: function (result){
+      var data = JSON.parse(result);
+      if (Array.isArray(result)) {
+      console.log(result);
+        parseData(result);
       }
-    }
-  });
+      else {
+        console.log('single object:');
+        console.log(result);
+        }
+      }
+    });
+
+  function parseData(arr) {
+    for( var x=0; x < arr.abilities.length; x++){
+      $('#result').append("<li id="+"id" + x +">" + (x+1) + "." +
+          "<li id=ability" + x + ">" + arr.abilities[x].ablity.name + "</li>");
+        }
+  }
   e.preventDefault();
-});
-
-
-
-   if ($('#pokemon').val()==''){	 }
-
-	 else {
-		 $('#result').append('<li> Whoops, the pokemon you entered are not here yet </li>');
-		 $('#result').append("<li>" + whatwhat + "</li>");
-	 }
-	 console.log("Caught submit event");
-	 e.preventDefault();
-
-});
+  });
